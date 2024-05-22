c248e2a11350b0a046677e7f4561c614c11ec44c
ar70n <107795054+ar70n@users.noreply.github.com>
2024-05-22 17:50:49 +0200

    Delete LetsCtf/ClickerBypass/sources directory

--- LetsCtf/ClickerBypass/sources/ctf/clickergame/BuildConfig.java
+++
@@ -1,9 +0,0 @@
-package ctf.clickergame;
-
-public final class BuildConfig {
-    public static final String APPLICATION_ID = "ctf.clickergame";
-    public static final String BUILD_TYPE = "release";
-    public static final boolean DEBUG = false;
-    public static final int VERSION_CODE = 1;
-    public static final String VERSION_NAME = "1.0";
-}
